98e4126eb1c5beda7fe48e087b621e047961f186
Ranj Jalal <ranj.jalal97@gmail.com>
2021-10-27 22:51:07 +0300

    Javascript functionalities added
    Added a group of functionalities to the project

--- script.js
+++ script.js
@@ -14,50 +14,70 @@ p1ScoreArea.textContent = p1Score;
 p2ScoreArea.textContent = p2Score;
 
 resetBtn.addEventListener("click", () => {
-  p1Score = 0;
-  p2Score = 0;
-  p1ScoreArea.textContent = p1Score;
-  p2ScoreArea.textContent = p2Score;
-  evaluateScore();
+    p1Score = 0;
+    p2Score = 0;
+    p1ScoreArea.textContent = p1Score;
+    p2ScoreArea.textContent = p2Score;
+    p1Btn.disabled = false;
+    p2Btn.disabled = false;
+    evaluateScore();
 });
 
 p1Btn.addEventListener("click", () => {
-  p1Score++;
-  p1ScoreArea.textContent = p1Score;
-  evaluateScore();
-  isWin();
+    p1Score++;
+    p1ScoreArea.textContent = p1Score;
+    evaluateScore();
+    isWin();
 });
 
 p2Btn.addEventListener("click", () => {
-  p2Score++;
-  p2ScoreArea.textContent = p2Score;
-  evaluateScore();
-  isWin();
+    p2Score++;
+    p2ScoreArea.textContent = p2Score;
+    evaluateScore();
+    isWin();
 });
 
 evaluateScore();
 // Evaluate score to change the color of the points based on who is winning
 function evaluateScore() {
-  if (p1Score === p2Score) {
-    p1ScoreArea.style.color = "black";
-    p2ScoreArea.style.color = "black";
-  } else if (p1Score > p2Score) {
-    p1ScoreArea.style.color = "green";
-    p2ScoreArea.style.color = "red";
-  } else if (p1Score < p2Score) {
-    p1ScoreArea.style.color = "red";
-    p2ScoreArea.style.color = "green";
-  }
+    if (p1Score === p2Score) {
+        p1ScoreArea.style.color = "black";
+        p2ScoreArea.style.color = "black";
+    } else if (p1Score > p2Score) {
+        p1ScoreArea.style.color = "green";
+        p2ScoreArea.style.color = "red";
+    } else if (p1Score < p2Score) {
+        p1ScoreArea.style.color = "red";
+        p2ScoreArea.style.color = "green";
+    }
 }
 
 function isWin() {
-  if (p1Score == scoreLimitNum) {
-    console.log(" P1 WIN ");
-  } else if (p2Score == scoreLimitNum) console.log(" P2 WIN ");
+    // TODO: TO add soome effect when either player won
+    if (p1Score == scoreLimitNum) {
+        console.log(" P1 WIN ");
+        p1Btn.disabled = true;
+        p2Btn.disabled = true;
+        resetAll();
+    } else if (p2Score == scoreLimitNum) {
+        console.log(" P2 WIN ");
+        p1Btn.disabled = true;
+        p2Btn.disabled = true;
+        resetAll();
+    }
+}
+
+function resetAll() {
+    p1Score = 0;
+    p2Score = 0;
+    p1ScoreArea.textContent = p1Score;
+    p2ScoreArea.textContent = p2Score;
+    p1Btn.disabled = false;
+    p2Btn.disabled = false;
+    evaluateScore();
 }
 
 function changeScoreLimit() {
-  console.log(scoreLimitNum);
-  scoreLimitNum = scoreLimit.value;
-  console.log(scoreLimit.value);
+    scoreLimitNum = scoreLimit.value;
+    resetAll();
 }
